e505aa7714332d46201529a79cd8544e56de7a53
Jason Huan <huan.jason@gmail.com>
2025-06-12 12:51:15 +0800

    2025-06-12-1251

--- taiping/models/course.py
+++ taiping/models/course.py
@@ -12,6 +12,7 @@ from .basemodel import BaseModel
 class Course(BaseModel):
     course_group = ForeignKey('taiping.CourseGroup', on_delete=PROTECT, null=True, blank=True)
     name = CharField(max_length=128, unique=True)
+    chinese_name = CharField(max_length=128, unique=True, null=True)
     description = TextField()
     default_course_fee = IntegerField(null=True, blank=True)
     default_min_students = IntegerField(null=True, blank=True)
