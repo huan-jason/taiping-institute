40981f981beda1eac3fb7522fe587faaf2ccce22
Jason Huan <huan.jason@gmail.com>
2025-06-24 22:08:05 +0800

    2025-06-24-2208

--- taiping/models/appdata.py
+++ taiping/models/appdata.py
@@ -9,5 +9,8 @@ class AppData(BaseModel):
     name = CharField(max_length=128, unique=True)
     data = TextField()
 
+    class Meta: # type: ignore
+        verbose_name_plural = "App Data"
+
     def __str__(self) -> str:
         return self.name
