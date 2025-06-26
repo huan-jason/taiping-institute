7fad2570d68b38e2923797491d331c6f9c8e1f84
Jason Huan <huan.jason@gmail.com>
2025-06-26 13:37:37 +0800

    2025-06-26-1337

---
+++ taiping/bin/course_auto_start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+
+readonly SCRIPT_DIR="$(dirname "$(readlink -f $0)")"
+readonly LOG_DIR="$SCRIPT_DIR/../../../logs"
+
+mkdir -p $LOG_DIR
+
+docker compose exec app ./manage.py course_auto_start >>$LOG_DIR/course_auto_start.log
