5d50deb5c6a036e3da37bb684cf61b2fe53d227d
Jason Huan <huan.jason@gmail.com>
2026-03-02 16:41:46 +0800

    2026-03-02-1641

--- taiping/views/indexview.py
+++ taiping/views/indexview.py
@@ -3,7 +3,7 @@ from typing import Any, cast
 from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
 
 from taiping.models import (
@@ -15,66 +15,25 @@ from taiping.utils import get_courses_list_context
 
 class IndexView(View):
 
-    TABS: dict[str, dict] = {
-        "instructor": {
-            "id": "instructor",
-            "label": "Instructor Dashboard",
-            "show": False,
-        },
-        "student": {
-            "id": "student",
-            "label": "Student Dashboard",
-            "show": False,
-        },
-        "courses": {
-            "id": "courses",
-            "label": "Courses",
-            "show": True,
-        },
-    }
-
     def get(self, request: HttpRequest) -> HttpResponse:
-        if not request.user.is_authenticated:
-            return redirect("course_list")
-
         if htmx := request.GET.get("htmx"):
-            return getattr(self, f"htmx_{request.GET["htmx"]}")(request)
+            return getattr(self, f"htmx_{htmx}")(request)
 
-        user: Any = self.get_user(request)
+        user: User | None = self.get_user(request)
         is_instructor: bool = hasattr(user, "instructor")
         is_student: bool = hasattr(user, "student")
-        tabs: dict[str, dict] = self.TABS
-        active_tab: str = ""
-        show_create_course_button: bool = (request.user.groups
-            .filter(name="data_admin")
-            .exists()
+
+        context: dict[str, Any] = (
+            get_courses_list_context(request, use_session_filters=True)
+            | dict(
+                is_instructor=is_instructor,
+                is_student=is_student,
+            )
         )
+        return render(request, "taiping/index/index.html", context)
 
-        if is_instructor:
-            active_tab = "instructor"
-            tabs["instructor"]["show"] = True
-
-        if is_student:
-            tabs["student"]["show"] = True
-            if not is_instructor:
-                active_tab = (
-                    "student"
-                    if Registration.objects.filter(student=user.student).exists()
-                    else "courses"
-                )
-
-        if tab := request.GET.get("tab"):
-            active_tab = tab
-
-        request.session["course_filters"] = {  # for htmx calls
-            key: request.GET[key]
-            for key in request.GET
-            if key.startswith("filter_")
-        }
-
-        return render(request, "taiping/dashboard/index.html", locals())
-
-    def get_user(self, request: HttpRequest) -> User:
+    def get_user(self, request: HttpRequest) -> User | None:
+        if not request.user.is_authenticated: return None
         user: User = cast(User, request.user)
         if not user.is_superuser: return user
         if not (username := request.GET.get("u")): return user
